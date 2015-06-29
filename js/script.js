759cd12d53762be4b9b153020c8c8c4ae02cda1b
swarley00 <jason.caryl@gmail.com>
2015-06-29 17:11:11 -0400

    Trying to get the Comic API call to log

--- js/script.js
+++ js/script.js
@@ -2,24 +2,39 @@ $(function () {
 
 	$('.search-btn').click(function (event) {
 		var myInput = $('.input-field').val();
-		apiCall(myInput);
+		getCharacters(myInput);
+		getComics();
 	});
 
-	function apiCall (inputName) {
+	function getCharacters (inputName) {
 		var characterAPI = 'https://gateway.marvel.com/v1/public/characters';
 		$.getJSON(characterAPI, {
 			apikey: '8f39c130c033ff3886d62c54d7a4ecb2',
 			name: inputName
 		})
 		.done(function (data) {
-			console.log(data);
 			var image = data.data.results[0].thumbnail;
 			var characterName = data.data.results[0].name;
+			var characterID = data.data.results[0].id;
 			$('.character-image').show();
 			$('.character-profile').attr("src", image.path + "." + image.extension);
 			$('.character-name').text(characterName);
 		});
 	}
+
+	function getComics () {
+		var characterID = ;
+		var comicsAPI = 'https://gateway.marvel.com/v1/public/characters/' + characterID + 'comics';
+		$.getJSON(comicsAPI, {
+			apikey: '8f39c130c033ff3886d62c54d7a4ecb2',
+		})
+		.done(function (data) {
+			console.log(data);
+		});
+	}
 });
 
 //data.data.results[0].id
+//var comicsAPI = 'https://gateway.marvel.com/v1/public/characters/1009664/comics';
+//How do I get the Character ID from one API call and use it in another API call?
+
