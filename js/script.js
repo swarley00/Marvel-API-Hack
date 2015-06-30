59b4bdf02f8b0a55be80e803fe622574baea8d7c
swarley00 <jason.caryl@gmail.com>
2015-06-30 17:21:04 -0400

    Get and display comic images and titles

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ $(function () {
 			name: inputName
 		})
 		.done(function (data) {
-			//console.log(data);
 			var image = data.data.results[0].thumbnail;
 			var characterName = data.data.results[0].name;
 			var characterID = data.data.results[0].id;
@@ -25,26 +24,23 @@ $(function () {
 
 	function getComics (characterID) {
 		var characterId = characterID;
-		var comicsAPI = 'https://gateway.marvel.com/v1/public/characters/' + characterId + 'comics';
+		var comicsAPI = 'https://gateway.marvel.com/v1/public/characters/' + characterId + '/comics';
 		$.getJSON(comicsAPI, {
-			//dateDescriptor: 'thisMonth',
 			apikey: '8f39c130c033ff3886d62c54d7a4ecb2'
 		})
 		.done(function (data) {
-			var characterName = data.data.results[0].name;
-			var comics = data.data.results[0].comics.items.length;
+			var characterName = data.data.results[0].name; //fix this
+			var comics = data.data.results.length;
 			console.log(data);
 			$('.comic-copy').show();
 			$('.comic-container').show();
-			$('.comic-description').text("Here are the most recent comic book titles " + characterName + " appears in.");
+			$('.comic-description').text("Here are the most recent comic book titles " + characterName + " appears in."); //fix this
 			for (var i=0; i < comics; i++) {
-				var comicTitle = data.data.results[0].comics.items[i].name;
-				console.log(comicTitle);
+				var image = data.data.results[i].thumbnail;
+				var comicTitle = data.data.results[i].title;
+				$('.comic').append("<img src='" + image.path + "." + image.extension + "' alt='Comic Image' height='324px' width='216px'>");
 				$('.comic').append("<p>" + comicTitle + "</p>");
-				// $('.comic-title').text(comicTitle);
 			}
 		});
 	}
 });
-
-//data.data.
