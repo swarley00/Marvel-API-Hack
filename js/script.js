e93da81ec662058aa19a2e2347c0a733a7ea3c31
swarley00 <jason.caryl@gmail.com>
2015-07-09 09:41:45 -0400

    Autocomplete code first attempt

--- js/script.js
+++ js/script.js
@@ -13,6 +13,33 @@ $(function () {
 		}
 	});
 
+	$('.input-field').autocomplete({
+		source: function (request, response) {
+			var characterAPI = 'https://gateway.marvel.com/v1/public/characters';
+			$.getJSON(characterAPI, {
+				limit: 20,
+				apikey: '8f39c130c033ff3886d62c54d7a4ecb2'
+			})
+			.done(function (data) {
+				var characterList = data.data.results.length;
+				for (var i = 0; i < characterList; i++) {
+					response(data.data.results[i].name);
+					console.log(data.data.results[i].name);
+				}
+			});
+		}
+	});
+	
+	// var availableCharacters = [
+	// 	"A.I.M.",
+	// 	"Aaron Stack",
+	// 	"Absorbing Man"
+	// ];
+
+	// $('.input-field').autocomplete({
+	// 	source: availableCharacters
+	// });
+
 	function getCharacters (inputName) {
 		var characterAPI = 'https://gateway.marvel.com/v1/public/characters';
 		$.getJSON(characterAPI, {
@@ -23,6 +50,7 @@ $(function () {
 			var image = data.data.results[0].thumbnail;
 			var characterName = data.data.results[0].name;
 			var characterID = data.data.results[0].id;
+			//console.log(data);
 			$('.character-image').show();
 			$('.character-profile').attr("src", image.path + "." + image.extension);
 			$('.character-name').text(characterName);
